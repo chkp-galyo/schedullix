cb95e1aa85ecf6124981568560159bf8c30df186
galyo9 <galyogev5@gmail.com>
2018-07-25 16:30:57 +0300

    date picker

--- src/views/edit-page.vue
+++ src/views/edit-page.vue
@@ -2,18 +2,18 @@
   <div class="edit-page">
       <header-cmp :headerConfig="user.configElements.header" v-if="user.configElements.header.isActive" />
       <about-cmp :workingHours="user.workingHours" :aboutConfig="user.configElements.about" v-if="user.configElements.about.isActive" /> 
+        <schedule-cmp :schedule="user.configElements.schedule" />
+        <edit-map-cmp :location="user.location" />
     {{user}}
   </div>
 </template>
 
 <script>
-<<<<<<< HEAD
-import headerCmp from '../components/editPage/edit-header-cmp.vue'
-import aboutCmp from '../components/editPage/edit-about-cmp.vue'
-=======
 import headerCmp from '@/components/editPage/edit-header-cmp.vue'
 import aboutCmp from '@/components/editPage/edit-about-cmp.vue'
->>>>>>> 2d61530f0f60aaa761a65c996eed4f317bccd6c9
+import scheduleCmp from '@/components/editPage/edit-schedule-cmp.vue'
+import editMapCmp from '@/components/editPage/edit-map-cmp.vue'
+
 
 import { GETTER_USER } from '../store/userModule.js'
 
@@ -26,7 +26,9 @@ export default {
   },
   components: {
       headerCmp,
-      aboutCmp
+      aboutCmp,
+      scheduleCmp,
+      editMapCmp
   }
 }
 </script>
