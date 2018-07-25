2d61530f0f60aaa761a65c996eed4f317bccd6c9
galyo9 <galyogev5@gmail.com>
2018-07-25 13:40:23 +0300

    about-cmp

--- src/views/edit-page.vue
+++ src/views/edit-page.vue
@@ -1,19 +1,14 @@
 <template>
   <div class="edit-page">
       <header-cmp :headerConfig="user.configElements.header" v-if="user.configElements.header.isActive" />
-      <about-cmp :aboutConfig="user.configElements.about" v-if="user.configElements.about.isActive" /> 
+      <about-cmp :workingHours="user.workingHours" :aboutConfig="user.configElements.about" v-if="user.configElements.about.isActive" /> 
     {{user}}
   </div>
 </template>
 
 <script>
-<<<<<<< HEAD
 import headerCmp from '@/components/editPage/edit-header-cmp.vue'
 import aboutCmp from '@/components/editPage/edit-about-cmp.vue'
-=======
-import headerCmp from '../components/editPage/edit-header-cmp.vue'
-import aboutCmp from '../components/editPage/edit-about-cmp.vue'
->>>>>>> a1b4d3e0c2673740bea54f0a00d350f91c8d1d4f
 
 import { GETTER_USER } from '../store/userModule.js'
 
