e8c400a6b7eb9d56ceb863247d3167ddf1485edf
Idan Hakim <idanhakim123@gmail.com>
2018-07-25 12:27:37 +0300

    update

---
+++ src/components/editPage/edit-header-cmp.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="edit-header" :style="headerConfig.styleObj">
+    <h1>{{headerConfig.titleText}}</h1>
+    <img :src="headerConfig.imgUrl"/>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "editHeader",
+  components: {},
+  props: ["headerConfig"],
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
+
+<style scoped lang="scss">
+</style>
