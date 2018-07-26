aed4f326e844cd49d49e936b6602c5dddca1ecd8
Din_Hazan <dinhazan7@gmail.com>
2018-07-26 19:13:13 +0300

    home page adding

--- src/views/home-page.vue
+++ src/views/home-page.vue
@@ -1,14 +1,38 @@
 <template>
   <div class="home">
-    <img src="../assets/logo.png">
+<home-header></home-header>
+<v-app light>
+    <v-content>
+    <home-main></home-main>
+<home-footer></home-footer>
+      
+    </v-content>
+  </v-app>
+ </div>
+      
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import homeHeader from '@/components/homePage/home-header-cmp.vue'
+import homeMain from '@/components/homePage/home-main-cmp.vue'
+import homeFooter from '@/components/homePage/home-footer-cmp.vue'
 export default {
   name: "home",
-  components: {}
+  components: {
+    homeHeader,
+    homeMain,
+    homeFooter
+  }
 };
 </script>
+
+<style lang="scss" scoped>
+.home {
+  margin: 10px;
+  display: flex;
+  flex-direction: column;
+}
+
+</style>
