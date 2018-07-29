ef4f6eeb0f675d5a690ea208ebe1707351c40454
Idan Hakim <idanhakim123@gmail.com>
2018-07-29 20:37:18 +0300

    update

--- src/components/editPage/edit-work-hours-cmp.vue
+++ src/components/editPage/edit-work-hours-cmp.vue
@@ -68,35 +68,7 @@ export default {
   data() {
     return {
       price: [8, 17],
-      workingHourEdit: JSON.parse(JSON.stringify(this.workingHours)),
-      hours: [
-        0,
-        1,
-        2,
-        3,
-        4,
-        5,
-        6,
-        7,
-        8,
-        9,
-        10,
-        11,
-        12,
-        13,
-        14,
-        15,
-        16,
-        17,
-        18,
-        19,
-        20,
-        21,
-        22,
-        23
-      ],
-      minutes: ["00", "15", "30", "45"],
-      days: [true, true, true, true, true, false, false]
+      workingHourEdit: JSON.parse(JSON.stringify(this.workingHours))
     };
   },
   created() {
